c3cc3b71694dfb1639fbcd942ad02b490d7a2bd8
Esra Varlık <40757395+esravarlik@users.noreply.github.com>
2022-10-20 19:45:53 +0300

    Update PrintGrid.java

--- PrintGrid.java
+++ PrintGrid.java
@@ -16,18 +16,55 @@ printGrid(5, 3) ➞ "
 
 //Solution;
 
-public class Challenge {
-	public static String printGrid(int rows, int cols) {
-		StringBuilder grid = new StringBuilder();
-		for (int i = 0; i < rows; i++) {
-			int n = i + 1;
-			for (int j = 0; j < cols - 1; j++) {
-				grid.append(n + ", ");
-				n += rows;
-			}
-			grid.append(n + "\n");
-		}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(printGridHorizontal(3,6));
+        System.out.println(printGridVertical(3,6));
+    }
 
-		return grid.toString().trim();
-	}
+    /*
+    printGridHorizontal;
+        1, 2, 3, 4, 5, 6
+        7, 8, 9, 10, 11, 12
+        13, 14, 15, 16, 17, 18
+     */
+
+    public static String printGridHorizontal(int rows, int cols) {
+        String a = "";
+        int col = 1;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(j == cols-1){
+                    a += col;
+                    col++;
+                }else{
+                    a += col + ", ";
+                    col++;
+                }
+            }
+            a += "\n";
+        }
+        return a;
+    }
+
+    /*
+    printGridVertical;
+        1, 4, 7, 10, 13, 16
+        2, 5, 8, 11, 14, 17
+        3, 6, 9, 12, 15, 18
+     */
+
+    public static String printGridVertical(int rows, int cols) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                sb.append(i + j * rows);
+                if (j == cols -1)
+                    sb.append("\n");
+                else
+                    sb.append(", ");
+            }
+        }
+        return sb.toString().trim();
+    }
 }
